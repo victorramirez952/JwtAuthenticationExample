f7c2e703f5d052f3e3d2d30bf90a51b58b6e276f
victorramirez952 <victory.ramirez@udem.edu>
2026-04-28 08:25:27 -0600

    feaT: Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿## JwtAuthenticationExample
+
+Proyecto MVC de ejemplo con autenticación JWT usando Microsoft.AspNetCore.Authentication.JwtBearer.
+
+El proyecto crea una base de datos en memoria para almacenar información.
+
+Las claves de autenticación están hardcodeadas en el código para simplificar el ejemplo.
--- obj/Debug/net10.0/AuthJWT.AssemblyInfo.cs
+++ obj/Debug/net10.0/AuthJWT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthJWT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59f8de5ca396694757b004dbd00547980ae57b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthJWT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthJWT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
